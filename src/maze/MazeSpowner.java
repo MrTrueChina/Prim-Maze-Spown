9fe69225d4902085158e007f288093a0af46fefe
Mr.True.China <mr.true.china@gmail.com>
2019-04-13 15:44:04 +0900

    Update MazeSpowner.java

--- src/maze/MazeSpowner.java
+++ src/maze/MazeSpowner.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class MazeSpowner { // AAA
+public class MazeSpowner {
     private static LinkedList<Point> _readyNodes;
     private static LinkedList<Point> _carvedNodes;
     private static Maze _maze;
